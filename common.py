c3e5d1c6bb23b67af97f5b6549ce68303c187cd3
ddowrs <whitezyg@gmail.com>
2020-03-25 16:05:43 +0800

    chore: add print color

---
+++ common.py
@@ -0,0 +1,15 @@
+class printColor:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def SuccessMsg(msg):
+    return printColor.OKGREEN + printColor.BOLD + msg + printColor.ENDC
+
+def ErrorMsg(msg):
+    return printColor.FAIL + printColor.BOLD + msg + printColor.ENDC
