96e0dd5514aea86f261c2566a4d952aa5c23d7f3
ddowrs <whitezyg@gmail.com>
2020-03-30 16:41:16 +0800

    refactor: modify data generator

--- data_generator.py
+++ data_generator.py
@@ -5,55 +5,78 @@ from common import MyJSONEncoder
 
 decimals = 2
 
-g_num = int(input("gene number: "))
 
-dist_low = int(input("distance low number: "))
-dist_high = int(input("distance high number: "))
+def create_gene_number():
+    return int(input("gene number: "))
 
-dist = np.random.randint(dist_low, dist_high, (g_num + 1, g_num + 1))
-i = [*range(g_num + 1)]
-dist[i, i] = 0
-dist = np.round((dist + dist.T) / 2, decimals)
 
-time_dist_ratio = float(input(("time cost and distance ratio: ")))
-time_cost = np.round(dist * time_dist_ratio, decimals)
+def create_dist_and_time_matrix(g_num):
+    dist_low = int(input("distance low number: "))
+    dist_high = int(input("distance high number: "))
 
-day_num = int(input("day number: "))
-everyday_time = float(input("everyday time: "))
-route_time = np.ones(day_num + 1) * everyday_time
-route_time[0] = 0
+    dist = np.random.randint(dist_low, dist_high, (g_num + 1, g_num + 1))
+    i = [*range(g_num + 1)]
+    dist[i, i] = 0
 
-play_time_low = int(input("play time low: "))
-play_time_high = int(input("play time high: "))
-play_time = np.round(
-    np.random.rand(g_num + 1) * (play_time_high - play_time_low) + play_time_low,
-    decimals,
-)
-play_time[0] = 0
+    dist = np.round((dist + dist.T) / 2, decimals)
 
-time_window = []
-for i in range(g_num + 1):
-    time_window.append(
-        sorted(
-            [
-                round(random.uniform(0, everyday_time), decimals),
-                round(random.uniform(0, everyday_time), decimals),
-            ]
-        )
+    time_dist_ratio = float(input("time cost and distance ratio: "))
+
+    return (
+        dist.tolist(),
+        np.round(dist * time_dist_ratio, decimals).tolist(),
     )
-time_window[0] = [0, 24]
 
-data = {
-    "g_num": g_num,
-    "dist": dist.tolist(),
-    "time_cost": time_cost.tolist(),
-    "route_time": route_time.tolist(),
-    "play_time": play_time.tolist(),
-    "time_window": time_window,
-}
 
-json_data = json.dumps(data, indent=4, cls=MyJSONEncoder)
+def create_day_time():
+    day_num = int(input("day number: "))
+    everyday_time = float(input("everyday time: "))
+    route_time = np.ones(day_num + 1) * everyday_time
+    route_time[0] = 0
+    return day_num, everyday_time, route_time.tolist()
 
-out_filename = input("generate filename: ")
-with open(f"{out_filename}.in.json", "w") as f:
-    f.write(json_data)
+
+def create_play_time(g_num):
+    play_time_low = int(input("play time low: "))
+    play_time_high = int(input("play time high: "))
+    play_time = np.round(
+        np.random.rand(g_num + 1) * (play_time_high - play_time_low) + play_time_low,
+        decimals,
+    )
+    play_time[0] = 0
+    return play_time.tolist()
+
+
+def create_time_window(g_num, everyday_time):
+    time_window = []
+    for _ in range(g_num + 1):
+        time_window.append(
+            sorted(
+                [
+                    round(random.uniform(0, everyday_time), decimals),
+                    round(random.uniform(0, everyday_time), decimals),
+                ]
+            )
+        )
+    time_window[0] = [0, 24]
+    return time_window
+
+
+def main():
+    data = {}
+
+    data["g_num"] = create_gene_number()
+    data["dist"], data["time_cost"] = create_dist_and_time_matrix(data["g_num"])
+    data["days"], everyday_time, data["route_time"] = create_day_time()
+    data["play_time"] = create_play_time(data["g_num"])
+    data["time_window"] = create_time_window(data["g_num"], everyday_time)
+
+    json_data = json.dumps(data, indent=4, cls=MyJSONEncoder)
+
+    out_filename = input("generate filename: ")
+    with open(f"{out_filename}.in.json", "w") as f:
+        f.write(json_data)
+
+
+if __name__ == "__main__":
+    main()
