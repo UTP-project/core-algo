42bc4868f2ca24ead2a1156844f98069c214efa0
ddowrs <whitezyg@gmail.com>
2020-04-07 18:19:55 +0800

    fix: add print of experiment table

--- exp_compare.py
+++ exp_compare.py
@@ -2,6 +2,7 @@ from GA import GA
 
 import json
 import time
+from tabulate import tabulate
 
 
 def average_res(ga, *params):
@@ -20,22 +21,33 @@ def average_res(ga, *params):
     return (fitness_sum / cal_times, runtime_sum / cal_times)
 
 
-def print_res(base, compare):
-    print(base)
+def print_table(base, compare, header, filename=""):
+    tabular_data = []
+    tabular_data.append(["base", *base])
     for k, v in compare.items():
-        print(k, v)
+        tabular_data.append([k, *v])
+    table = tabulate(tabular_data, headers=["", *header])
+    print(table)
+
+    if filename:
+        with open(filename, "w") as f:
+            f.write(table)
 
 
 def param_compare():
-    # use default 20 points data
-    with open("test_20_0.in.json") as f:
+    # get test data filename
+    data = {}
+    filename = input("test data file: ")
+    with open(f"{filename}.in.json") as f:
         data = json.load(f)
     # offspring_percent, recovery_rate, pop_num=50, pfih_rate=0, rws_rate=0.5, iteration=500
     base_params = [1, 0.04, 50, 0, 1, 500]
     compare_params_dict = {
         "00-recovery": [1, 0, 50, 0, 1, 500],
+        "02-recovery": [1, 0.02, 50, 0, 1, 500],
+        "06-recovery": [1, 0.06, 50, 0, 1, 500],
         "08-recovery": [1, 0.08, 50, 0, 1, 500],
-        "12-recovery": [1, 0.12, 50, 0, 1, 500],
+        "10-recovery": [1, 0.10, 50, 0, 1, 500],
         "30-pop": [1, 0.04, 30, 0, 1, 500],
         "40-pop": [1, 0.04, 40, 0, 1, 500],
         "60-pop": [1, 0.04, 60, 0, 1, 500],
@@ -56,8 +68,17 @@ def param_compare():
     base_res = average_res(ga, *base_params)
     for k, params in compare_params_dict.items():
         tmp = average_res(ga, *params)
-        compare_res[k] = (tmp[0] - base_res[0], tmp[1] - base_res[1])
-    print_res(base_res, compare_res)
+        fitness_diff = tmp[0] - base_res[0]
+        runtime_diff = tmp[1] - base_res[1]
+        compare_res[k] = (
+            round(fitness_diff, 2),
+            round(fitness_diff * 100 / base_res[0], 2),
+            round(runtime_diff, 2),
+            round(runtime_diff * 100 / base_res[1], 2),
+        )
+    base_res = (round(base_res[0], 2), 0, round(base_res[1], 2), 0)
+    header = ["fitness", "fitness growth(%)", "runtime", "runtime growth(%)"]
+    print_table(base_res, compare_res, header, f"{filename}.tbl")
 
 
 def main():
